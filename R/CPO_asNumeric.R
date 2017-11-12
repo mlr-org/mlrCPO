7eb9d7a83819459973043728500668db9a64e569
mb706 <ma.binder@campus.lmu.de>
2017-11-12 10:23:59 +0100

    cpoAsNumeric has its own file

---
+++ R/CPO_asNumeric.R
@@ -0,0 +1,13 @@
+
+#' @title Convert All Features to Numerics
+#'
+#' @template cpo_description
+#'
+#' @template arg_cpo_id
+#' @family CPO
+#' @export
+cpoAsNumeric = makeCPOExtended("as.numeric", .properties.adding = c("factors", "ordered"), .properties.needed = "numerics",  # nolint
+  .trafo.type = "stateless", .dataformat = "factor", cpo.trafo = function(data, target) {
+    as.data.frame(lapply(data, as.numeric), row.names = rownames(data)) }, cpo.retrafo = function(data) {
+      as.data.frame(lapply(data, as.numeric), row.names = rownames(data)) })
+registerCPO(cpoCollapseFact, "data", "feature conversion", "Convert all Features to Numerics using as.numeric.")
--- R/CPO_concrete.R
+++ R/CPO_concrete.R
@@ -1,17 +1,4 @@
 
-#' @title Convert All Features to Numerics
-#'
-#' @template cpo_description
-#'
-#' @template arg_cpo_id
-#' @family CPO
-#' @export
-cpoAsNumeric = makeCPOExtended("as.numeric", .properties.adding = c("factors", "ordered"), .properties.needed = "numerics",  # nolint
-  .trafo.type = "stateless", .dataformat = "factor", cpo.trafo = function(data, target) {
-    as.data.frame(lapply(data, as.numeric), row.names = rownames(data)) }, cpo.retrafo = function(data) {
-      as.data.frame(lapply(data, as.numeric), row.names = rownames(data)) })
-registerCPO(cpoCollapseFact, "data", "feature conversion", "Convert all Features to Numerics using as.numeric.")
-
 #' @title Construct a CPO for scaling / centering
 #'
 #' @template cpo_description
