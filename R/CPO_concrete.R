b2968109a7a7fdbbe04ca99f67b9e0760c191540
mb706 <ma.binder@campus.lmu.de>
2017-11-12 10:17:27 +0100

    cpoSpatialSign has its own file

--- R/CPO_concrete.R
+++ R/CPO_concrete.R
@@ -1,31 +1,4 @@
 
-#' @title Scale Rows to Unit Length
-#'
-#' @description
-#' Normalizes the data row-wise. This is a natural
-#' generalization of the "sign" function to higher
-#' dimensions.
-#'
-#' @template cpo_description
-#'
-#' @param length [\code{numeric(1)}]\cr
-#'   Length to scale rows to. Default is 1.
-#'
-#' @template arg_cpo_id
-#' @family CPO
-#' @export
-cpoSpatialSign = makeCPOExtended("spatial.sign", length = 1: numeric[0, ~.], .dataformat = "numeric", .trafo.type = "stateless",  # nolint
-  .properties = c("numerics", "factors", "ordered"),  # no missings
-  cpo.trafo = {
-    t(apply(as.matrix(data), 1, function(x) {
-      len = sqrt(sum(x ^ 2))
-      if (!identical(len, 0)) {
-        x = x / len * length
-      }
-      x
-    }))
-  }, cpo.retrafo = NULL)
-registerCPO(cpoSpatialSign, "data", "numeric data preprocessing", "Scale numeric rows to given length.")
 
 #' @title Probability Encoding
 #'
