fec10cfdd1a0ea0074e6697f3296d375ff2d7b5e
mb706 <ma.binder@campus.lmu.de>
2017-11-29 14:59:03 +0100

    shorter variables in compositeProperties()

--- R/FormatCheck.R
+++ R/FormatCheck.R
@@ -29,7 +29,6 @@
 # @return [list] the data to feed to the CPO trafo function, as well as meta-information:
 #   list(indata = list(data, target [, data.reduced, target.reduced]), shapeinfo, properties)
 prepareTrafoInput = function(indata, dataformat, strict.factors, get.data.reduced, allowed.properties, subset.selector, capture.factors, name) {
-  stop("TODO")
   assert(checkClass(indata, "data.frame"), checkClass(indata, "Task"))
   if ("Task" %in% class(indata)) {
     targets = getTaskTargetNames(indata)
@@ -342,39 +341,39 @@ getTaskProperties = function(data) {
 # @name1 [character(1)] name of CPO1 to print in debug messages
 # @name2 [character(1)] name of CPO2 to print in debug messages
 # @return [list of character] properties of compound CPO, with same slots as `prop1`
-# returns a list(properties, properties.adding, properties.needed)
+# returns a list(handling, adding, needed)
 compositeProperties = function(prop1, prop2, name1, name2) {
-  properties.handling.1 = prop1$handling
-  properties.adding.1 = prop1$adding
-  properties.needed.1 = prop1$needed
-  properties.handling.2 = prop2$handling
-  properties.adding.2 = prop2$adding
-  properties.needed.2 = prop2$needed
-  assertCharacter(properties.handling.1, unique = TRUE)
-  assertCharacter(properties.handling.2, unique = TRUE)
-  assertCharacter(properties.adding.1, unique = TRUE)
-  assertCharacter(properties.adding.2, unique = TRUE)
-  assertCharacter(properties.needed.1, unique = TRUE)
-  assertCharacter(properties.needed.2, unique = TRUE)
+  handling.1 = prop1$handling
+  adding.1 = prop1$adding
+  needed.1 = prop1$needed
+  handling.2 = prop2$handling
+  adding.2 = prop2$adding
+  needed.2 = prop2$needed
+  assertCharacter(handling.1, unique = TRUE)
+  assertCharacter(handling.2, unique = TRUE)
+  assertCharacter(adding.1, unique = TRUE)
+  assertCharacter(adding.2, unique = TRUE)
+  assertCharacter(needed.1, unique = TRUE)
+  assertCharacter(needed.2, unique = TRUE)
   assertString(name1)
   assertString(name2)
   # some explanation about properties:
-  # * 'properties.handling' are the properties that a CPO can handle.
-  # * 'properties.adding' are the properties it adds to the things coming after it, it is therefore
+  # * 'handling' are the properties that a CPO can handle.
+  # * 'adding' are the properties it adds to the things coming after it, it is therefore
   #   the things it *removes* from a dataset. E.g. if it removes 'missings' from data, it adds the property
   #   'missings' to the pipeline.
-  # * properties.needed are the properties it needs from the things coming after it, this are the
+  # * needed are the properties it needs from the things coming after it, this are the
   #   the things it *adds* to a dataset. E.g. if it converts numerics to factors, it 'needs' the learner /
   #   CPOs coming after it to have the property 'factors'.
 
   # The conditions on the properties are:
-  # A) properties.adding is a subset of properties.handling
-  # B) properties.adding and properties.needed have no common elements
+  # A) adding is a subset of handling
+  # B) adding and needed have no common elements
   # (these should be checked upon creation of a CPO)
 
   # When composing two CPOs (CPO1 %>>% CPO2), there is an additional requirement:
-  # * properties.needed.1 is a subset of properties.handling.2
-  missing.properties = setdiff(properties.needed.1, properties.handling.2)
+  # * needed.1 is a subset of handling.2
+  missing.properties = setdiff(needed.1, handling.2)
   if (isPropertyStrict() && length(missing.properties)) {
     stopf("CPO %s creates data with propert%s %s that %s can not handle.",
       name1, ifelse(length(missing.properties) > 1, "ies", "y"),
@@ -383,26 +382,26 @@ compositeProperties = function(prop1, prop2, name1, name2) {
   }
 
   # The properties of the new CPO are obtained thus:
-  properties.composite = intersect(properties.handling.1, union(properties.handling.2, properties.adding.1))
-  properties.adding.composite = union(setdiff(properties.adding.1, properties.needed.2), intersect(properties.handling.1, properties.adding.2))
-  properties.needed.composite = union(setdiff(properties.needed.1, properties.adding.2), properties.needed.2)
+  composite = intersect(handling.1, union(handling.2, adding.1))
+  adding.composite = union(setdiff(adding.1, needed.2), intersect(handling.1, adding.2))
+  needed.composite = union(setdiff(needed.1, adding.2), needed.2)
 
   # Proofs that conditions (A) and (B) are still fulfilled:
-  # A) using distribution of union and intersect, and the fact that (cond (A)) intersect(properties.adding.1, properties.handling.1) == properties.adding.1,
+  # A) using distribution of union and intersect, and the fact that (cond (A)) intersect(adding.1, handling.1) == adding.1,
   #    we rewrite
-  #      properties.composite = union(properties.adding.1, intersect(properties.handling.1, properties.handling.2))
-  #    Now the first term in the union of properties.adding.composite is a subset of the first term of the union of properties.composite;
+  #      composite = union(adding.1, intersect(handling.1, handling.2))
+  #    Now the first term in the union of adding.composite is a subset of the first term of the union of composite;
   #    same with the second term of both.
-  # B) We show that intersect(properties.adding.composite, properties.needed.composite) is empty by showing that both terms in the union
-  #    of properties.adding.composite have empty intersect each with both terms in the union of properties.needed.composite.
-  #    1) intersect(properties.adding.1 - properties.needed.2, properties.needed.2) is empty because properties.needed.2 is subtracted from the lhs
-  #    2) intersect(properties.adding.1 - properties.needed.2, properties.needed.1 - properties.adding.2) is empty because
-  #       intersect(properties.adding.1, properties.needed.1) is empty per condition (B)
-  #    3) intersect(intersect(properties.handling.1, properties.adding.2), properties.needed.1 - properties.adding.2) is empty because properties.adding.2
+  # B) We show that intersect(adding.composite, needed.composite) is empty by showing that both terms in the union
+  #    of adding.composite have empty intersect each with both terms in the union of needed.composite.
+  #    1) intersect(adding.1 - needed.2, needed.2) is empty because needed.2 is subtracted from the lhs
+  #    2) intersect(adding.1 - needed.2, needed.1 - adding.2) is empty because
+  #       intersect(adding.1, needed.1) is empty per condition (B)
+  #    3) intersect(intersect(handling.1, adding.2), needed.1 - adding.2) is empty because adding.2
   #       is subtracted from the rhs
-  #    4) intersect(intersect(properties.handling.1, properties.adding.2), properties.needed.2) is empty because properties.needed.2 and properties.adding.2
+  #    4) intersect(intersect(handling.1, adding.2), needed.2) is empty because needed.2 and adding.2
   #       have empty intersect per condition (B)
-  list(handling = properties.composite, adding = properties.adding.composite, needed = properties.needed.composite)
+  list(handling = composite, adding = adding.composite, needed = needed.composite)
 }
 
 # give error when shape is different than dictated by shapeinfo.
