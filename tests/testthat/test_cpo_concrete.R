74b983dfeefa4fa49db816536e893472ffb590c1
mb706 <mlr.developer@mb706.com>
2019-02-17 17:36:16 +0100

    lint fix

--- tests/testthat/test_cpo_concrete.R
+++ tests/testthat/test_cpo_concrete.R
@@ -30,16 +30,16 @@ test_that("cpoScale test", {
 
   # scale of constant features works
   cfdf = data.frame(a = rep(1, 10), b = rep(0, 10))
-  cfdf_centered = data.frame(a = rep(0, 10), b = rep(0, 10))
-  cfdf_scaled = data.frame(a = rep(sqrt(0.9), 10), b = rep(0, 10))
-  expect_equal({cdx = cfdf %>>% cpoScale()}, cfdf_centered, check.attributes = FALSE)
-  expect_equal(cfdf %>>% retrafo(cdx), cfdf_centered, check.attributes = FALSE)
+  cfdf.centered = data.frame(a = rep(0, 10), b = rep(0, 10))
+  cfdf.scaled = data.frame(a = rep(sqrt(0.9), 10), b = rep(0, 10))
+  expect_equal({cdx = cfdf %>>% cpoScale()}, cfdf.centered, check.attributes = FALSE)
+  expect_equal(cfdf %>>% retrafo(cdx), cfdf.centered, check.attributes = FALSE)
 
-  expect_equal({cdx = cfdf %>>% cpoScale(scale = FALSE)}, cfdf_centered, check.attributes = FALSE)
-  expect_equal(cfdf %>>% retrafo(cdx), cfdf_centered, check.attributes = FALSE)
+  expect_equal({cdx = cfdf %>>% cpoScale(scale = FALSE)}, cfdf.centered, check.attributes = FALSE)
+  expect_equal(cfdf %>>% retrafo(cdx), cfdf.centered, check.attributes = FALSE)
 
-  expect_equal({cdx = cfdf %>>% cpoScale(center = FALSE)}, cfdf_scaled, check.attributes = FALSE)
-  expect_equal(cfdf %>>% retrafo(cdx), cfdf_scaled, check.attributes = FALSE)
+  expect_equal({cdx = cfdf %>>% cpoScale(center = FALSE)}, cfdf.scaled, check.attributes = FALSE)
+  expect_equal(cfdf %>>% retrafo(cdx), cfdf.scaled, check.attributes = FALSE)
 
   expect_equal({cdx = cfdf %>>% cpoScale(scale = FALSE, center = FALSE)}, cfdf, check.attributes = FALSE)
   expect_equal(cfdf %>>% retrafo(cdx), cfdf, check.attributes = FALSE)
