7bbefdd6ce59c8a92cc83cca7f5db4acaf6495b6
mb706 <mlr.developer@mb706.com>
2022-10-18 23:18:37 +0200

    use FNN instead of DiscriMinder for testing of learners that can't do prob

--- tests/testthat/test_core_targetop.R
+++ tests/testthat/test_core_targetop.R
@@ -490,14 +490,14 @@ test_that("composing CPO, CPOTrained with conversion etc. behaves as expected", 
 
   expect_set_equal(intersect(getLearnerProperties(se.from.prob %>>% makeLearner("classif.logreg")), c("prob", "se")), "se")
 
-  expect_set_equal(intersect(getLearnerProperties(se.from.prob %>>% makeLearner("classif.geoDA")), c("prob", "se")), character(0))
+  expect_set_equal(intersect(getLearnerProperties(se.from.prob %>>% makeLearner("classif.fnn")), c("prob", "se")), character(0))
 
   expect_set_equal(intersect(getLearnerProperties(se.remover %>>% (se.from.prob %>>% makeLearner("classif.logreg"))), c("prob", "se")), character(0))
 
   expect_equal(getLearnerType(se.from.prob %>>% makeLearner("classif.logreg")), "regr")
   expect_equal(getLearnerType(prob.from.response %>>% makeLearner("classif.logreg")), "classif")
 
-  expect_error(se.remover %>>% (se.from.prob %>>% makeLearner("classif.geoDA")), "for 'response' prediction, the Learner must have 'prob' prediction")
+  expect_error(se.remover %>>% (se.from.prob %>>% makeLearner("classif.fnn")), "for 'response' prediction, the Learner must have 'prob' prediction")
 
   expect_error(se.remover %>>% makeLearner("regr.ctree"), "for 'response' prediction, the Learner must have 'se' prediction")
 
